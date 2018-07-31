bb9c0eb71718b7d78b1ec2bc4fa48b3324b832dc
donank <11donank22@gmail.com>
2018-07-31 15:09:52 +0530

    created assets

--- core/src/com/mgiep/kaankholbubble/Screens/LevelSelectScreen.java
+++ core/src/com/mgiep/kaankholbubble/Screens/LevelSelectScreen.java
@@ -1,8 +1,22 @@
 package com.mgiep.kaankholbubble.Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.mgiep.kaankholbubble.KaankholBubble;
 
 public class LevelSelectScreen implements Screen {
+
+    final KaankholBubble game;
+    OrthographicCamera camera;
+
+    public LevelSelectScreen(KaankholBubble game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 480, 800);
+    }
+
     @Override
     public void show() {
 
@@ -10,7 +24,20 @@ public class LevelSelectScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
     }
 
     @Override
